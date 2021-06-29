13197d333fceab9d0db3b8f7ced433c0de2ebca2
ignacioblanc <iiblanc@falabella.cl>
2021-06-29 10:58:16 -0400

    lenguage and styles conf

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -3,17 +3,34 @@ import { NavBar } from '../ui/NavBar'
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-
+import "moment/locale/es";
+import {messages} from '../../helpers/calendar-message-es';
+moment.locale('es');
 const localizer = momentLocalizer(moment);
 
 const events = [{
     title: 'Cumpleaños del jefe',
     start: moment().toDate(),
     end: moment().add(2, 'hour').toDate(),
-    bgcolor: '#fafafa'
+    bgcolor: '#fafafa',
+    notes: 'Comprar el pastel'
 }]
 
 export const CalendarScreen = () => {
+
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+        return {
+            style
+        }
+
+    }
     return (
         <div className="calendar-screen">
             <NavBar /> 
@@ -22,6 +39,8 @@ export const CalendarScreen = () => {
                   events={events}
                   startAccessor="start"
                   endAccessor="end"
+                  messages={messages}
+                  eventPropGetter={eventStyleGetter}
                 />
         </div>
     )
